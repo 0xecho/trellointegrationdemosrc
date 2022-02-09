648f807630062def758a49d50c9f275e569786c2
0xecho <0xecho@proto-code.com>
2022-02-09 21:05:50 +0300

    Fix type

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,5 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
   },
-  publicPath: process.env.NODE_ENV === 'production' ? 'trellointegrationdemo/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? 'TrelloIntegrationdemo/' : '/'
 }
